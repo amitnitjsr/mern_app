9f287cd0f750ec421107a10f099bc7525bae3ad9
amitnitjsr <amitnips007@gmail.com>
2021-05-08 22:34:56 +0530

    define routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-// @route GET api/profile
+// @route GET api/posts
 // @desc Test Route
 // @access Public
-router.get('/', (req, res) => res.send('Profile Route'));
+router.get('/', (req, res) => res.send('Posts Route'));
 
 
 module.exports = router;
