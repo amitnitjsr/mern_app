b254c329c0508ecff62a400699402bfdfeb7d240
amitnitjsr <amitnips007@gmail.com>
2021-05-09 01:39:34 +0530

    added login api

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator/check');
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
 
@@ -18,5 +22,51 @@ router.get('/', auth, async (req, res) => {
 }
 );
 
+// @route POST api/auth
+// @desc Authenticate user and get token
+// @access Public
+router.post('/', [
+    check('email', 'Please include valid email').isEmail(),
+    check('password', 'Password is required')
+        .exists()
+],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const { email, password } = req.body;
+        try {
+            // check user exists
+            let user = await User.findOne({ email });
+            if (!user) {
+                return res.status(400).json({ errors: [{ msg: 'Invalid credentials' }] });
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+                return res.status(400).json({ errors: [{ msg: 'Invalid credentials' }] });
+            }
+
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            jwt.sign(payload, config.get('jwtSecret'),
+                { expiresIn: 36000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token })
+                });
+
+            // res.send('User created')
+        }
+        catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Errors');
+        }
+    });
 
 module.exports = router;
