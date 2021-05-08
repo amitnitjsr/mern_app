d3a648e08a70ad3c97a2509696c500d76275a020
amitnitjsr <amitnips007@gmail.com>
2021-05-08 23:23:45 +0530

    user created api added

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,10 +1,59 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator/check');
+const User = require('../../models/User');
 
-// @route GET api/users
-// @desc Test Route
+// @route POST api/users
+// @desc Register user
 // @access Public
-router.get('/', (req, res) => res.send('User Route'));
+router.post('/', [
+    check('name', 'Name is required')
+        .not()
+        .isEmpty(),
+    check('email', 'Please include valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters')
+        .isLength({ min: 6 })
+],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const { name, email, password } = req.body;
+        try {
+            // check user exists
+            let user = await User.findOne({ email });
+            if (user) {
+                return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+            }
+
+            const avatar = gravatar.url(email, {
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            })
+
+            user = new User({
+                name,
+                email,
+                avatar,
+                password
+            });
+
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+            await user.save();
+            res.send('User created')
+        }
+        catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Errors');
+        }
+
+        res.send('User Route');
+    });
 
 
 module.exports = router;
